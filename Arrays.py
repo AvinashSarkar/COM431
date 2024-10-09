203797e11b379d2e56bb522aa45556bdebe732ad
Avi Sarkar <2sarka38@solent.ac.uk>
2024-10-09 14:00:03 +0100

    Arrays.

---
+++ Arrays.py
@@ -0,0 +1,6 @@
+import numpy as np
+fruits = np.array(['Apple', 'Orange', 'Cherry'])
+
+print("The first fruit is " + str(fruits[0]))
+print("The second fruit is " + str(fruits[1]))
+print("The third fruit is " + str(fruits[2]))
