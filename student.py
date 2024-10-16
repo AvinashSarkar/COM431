555a8cb8a2de9b08f65266ee104989a0ccbdb91c
Avi Sarkar <2sarka38@solent.ac.uk>
2024-10-16 15:30:43 +0100

    Added object-oriented programming in student grades.

--- student.py
+++ student.py
@@ -1,1 +1,27 @@
-class student:
+class Student:
+    def __init__(self, student_id, name, course, mark):
+        self.student_id = student_id
+        self.name = name
+        self.course = course
+        self.mark = mark
+        self.is_valid_mark = self.validate_mark()
+        self.grade = self.determine_grade()
+
+    def validate_mark(self):
+        return 0 <= self.mark <= 100
+
+    def determine_grade(self):
+        if 0 <= self.mark <= 40:
+            return "Fail"
+        elif 40 <= self.mark <= 50:
+            return "Third"
+        elif 50 <= self.mark <= 60:
+            return "Lower Second"
+        elif 60 <= self.mark <= 70:
+            return "Upper Second"
+        elif 70 <= self.mark <= 80:
+            return "First"
+
+    def print_details(self):
+        print(f"ID: {self.student_id}, Name: {self.name}, Course: {self.course}"
+        f"Mark: {self.mark}, valid mark: {self.is_valid_mark}, grade: {self.grade}")
